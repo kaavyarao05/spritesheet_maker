dc69c20440e132cb15791c6602bfa45c2dbb3b77
kaavya <kaavyarao05@gmail.com>
2024-02-22 17:42:17 +0530

    closed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spritesheet_maker
 combines separate images into a spritesheet
+
+place images to combine in the "input" folder and when the combiner.py is run the spritesheet will be saved in the "output" folder.
---
+++ combiner.py
@@ -0,0 +1,40 @@
+import os,math
+from PIL import Image
+
+input_file_path="input"
+output_file_path="output"
+
+file_names=os.listdir(input_file_path)
+
+spritesheet_width=2
+
+input_file_array=[]
+x_size_array=[]
+y_size_array=[]
+
+for image_name in file_names:
+    image=Image.open(os.path.join(input_file_path,image_name))
+    size=image.size
+    input_file_array.append(image)
+    x_size_array.append(size[0])
+    y_size_array.append(size[1])
+
+cell_size=(max(x_size_array),max(y_size_array))
+sprite_number=len(input_file_array)
+
+x=(spritesheet_width)*cell_size[0]
+y=(math.ceil(sprite_number/spritesheet_width))*cell_size[1]
+
+output=Image.new("RGBA",(x,y),(0,0,0,0))
+
+pointer_x=0
+pointer_y=0
+for image in input_file_array:
+    if pointer_x==spritesheet_width:
+        pointer_x=0
+        pointer_y+=1
+    image_y_offset=cell_size[1]-image.size[1]  #so the image is placed at the bottom right of each cell
+    output.paste(image,(pointer_x*cell_size[0],pointer_y*cell_size[1]+image_y_offset))
+    pointer_x+=1
+
+output.save(os.path.join(output_file_path,"output.png"))
